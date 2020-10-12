005e5ff18dedfc642e431702a8002ea775aa92e6
ambisinistra <9372109@gmail.com>
2020-10-12 23:49:07 +0300

    added SQL for chats

--- SQLclasses.py
+++ SQLclasses.py
@@ -2,7 +2,7 @@ import sqlite3
 import datetime
 
 class SQLmessages:
-    def __init__(self, database_name : str):
+    def __init__(self, database_name="messages"):
         self.connection = sqlite3.connect(database_name)
         self.name = database_name
         self.cursor = self.connection.cursor()
@@ -38,5 +38,40 @@ class SQLmessages:
         """ Закрываем текущее соединение с БД """
         self.connection.close()
 
-test_class = SQLmessages("test_name")
-print(len(test_class))
+class SQLchatsGreatings:
+    def __init__(self, database_name="chats_ifno"):
+        self.connection = sqlite3.connect(database_name)
+        self.name = database_name
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS {} (chat_id integer, animation_link text, capcha bool, begin_date datetime)".format(self.name))
+
+    def record_by_chat_id(self, chat_id: int):
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM {} WHERE  chat_id = '{}';".format(self.name, chat_id)).fetchall()
+            if len(result) > 1:
+                print ("WARNING: for this chat more that one records")
+            return (result)
+    
+    def delete_record(self, chat_id: int):
+        """ Удаляем все строки с таким chat_id"""
+        with self.connection:
+            result = self.cursor.execute("DELETE FROM {} WHERE chat_id = '{}' AND message_id = '{}';".format(self.name, chat_id))
+            return (result)
+
+    def update_record(self, chat_id: int, animation_link: str, capcha: bool):
+        with self.connection:
+            current_time = datetime.datetime.now()
+            current_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+            print ("UPDATING record for CHAT", chat_id)
+            if len(record_by_chat_id(chat_id) > 0):
+                print ("THERE was record for", chat_id)
+                delete_record(chat_id)
+                print ("IT was DELETED")
+            self.cursor.execute("INSERT INTO {}(chat_id, animation_link, capcha, begin_date) VALUES(?,?,?,?);".format(self.name), (chat_id, animation_link, capcha, current_time))
+        return (0)
+
+    def __len__(self):
+        """ Считаем количество строк """
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM {};".format(self.name)).fetchall()
+            return len(result)
